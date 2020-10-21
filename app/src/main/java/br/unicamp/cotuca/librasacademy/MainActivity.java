edbff9e65d44447cd5415b0ffb06b0380c8c52cd
NicolasM3 <niicck992@gmail.com>
2020-10-21 08:43:29 -0300

    custom list

--- app/src/main/java/br/unicamp/cotuca/librasacademy/MainActivity.java
+++ app/src/main/java/br/unicamp/cotuca/librasacademy/MainActivity.java
@@ -1,7 +1,18 @@
 package br.unicamp.cotuca.librasacademy;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 
@@ -11,6 +22,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawer;
+    private ListView listview;
+    String mtitle[] = {"Alfabeto Manual"};
+    String mDescription[] = {"Que tal aprende sobre o alfabeto utilizade na libras"};
+    String mAuthor[] = {"Desconhecido"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +40,58 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        listview = findViewById(R.id.list_itens);
+
+        MyAdapter adapter = new MyAdapter(this, mtitle, mDescription, mAuthor);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0)
+                {
+                    Toast.makeText(MainActivity.this, "clicou", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    class MyAdapter extends ArrayAdapter<String>
+    {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        String rAuthor[];
+        
+        MyAdapter(Context c, String title[], String description[], String author[])
+        {
+            super(c, R.layout.item_row, R.id.text_title, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rAuthor = author;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.item_row, parent, false);
+            TextView myTitle = row.findViewById(R.id.text_title);
+            TextView myDescription = row.findViewById(R.id.text_description);
+            TextView myAuthor = row.findViewById(R.id.text_author);
+
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+            myAuthor.setText(rAuthor[position]);
+
+
+            return row;
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -35,5 +101,4 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
 }
