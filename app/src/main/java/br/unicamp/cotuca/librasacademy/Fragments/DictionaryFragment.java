a2d0b47ed5709ef05de8cdcfc0648893c36b5741
NicolasM3 <niicck992@gmail.com>
2020-11-11 12:23:59 -0300

    tela de palavras

--- app/src/main/java/br/unicamp/cotuca/librasacademy/Fragments/DictionaryFragment.java
+++ app/src/main/java/br/unicamp/cotuca/librasacademy/Fragments/DictionaryFragment.java
@@ -1,18 +1,24 @@
 package br.unicamp.cotuca.librasacademy.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONObject;
 
@@ -23,11 +29,14 @@ import java.util.HashMap;
 import br.unicamp.cotuca.librasacademy.HttpManager;
 import br.unicamp.cotuca.librasacademy.R;
 import br.unicamp.cotuca.librasacademy.VolleyCallback;
+import br.unicamp.cotuca.librasacademy.WordActivity;
 
 public class DictionaryFragment extends Fragment {
     ArrayList<String> alphabet = new ArrayList(Arrays.asList(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}));
     private Context context;
     private ListView itens;
+    private ImageButton search;
+    private EditText txtSearch;
 
     private String server;
 
@@ -48,6 +57,8 @@ public class DictionaryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         itens = (ListView) view.findViewById(R.id.list_dictionary);
+        search = (ImageButton) view.findViewById(R.id.buttonSearch);
+        txtSearch = (EditText) view.findViewById(R.id.edtTextSerach);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>( context, android.R.layout.simple_list_item_1, alphabet);
 
@@ -55,9 +66,41 @@ public class DictionaryFragment extends Fragment {
         itens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//              Toast.makeText(context, "Letras", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "Letras", Toast.LENGTH_SHORT).show();
                 attList(alphabet.get(i));
+            }
+        });
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("word", txtSearch.getText().toString());
+
+                try {
+                    HttpManager.get(context, server + ":5000/getGif/", params, new VolleyCallback() {
+                        @Override
+                        public void onSuccess(JSONObject result) {
+                            String txt = result.toString();
+
+                            Toast.makeText(context, "Palavra encontrada", Toast.LENGTH_SHORT).show();
+
+                            Intent myIntent = new Intent(getActivity(), WordActivity.class);
+                            Bundle b = new Bundle();
+                            b.putString("word", txtSearch.getText().toString());
+                            myIntent.putExtras(b);
+                            getActivity().startActivity(myIntent);
+                        }
+
+                        @Override
+                        public void onError(JSONObject result) {
+                            Toast.makeText(context, "Deu errado", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
         });
     }
---
+++ app/src/main/java/br/unicamp/cotuca/librasacademy/WordActivity.java
@@ -0,0 +1,85 @@
+package br.unicamp.cotuca.librasacademy;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class WordActivity extends AppCompatActivity {
+    private String server;
+    private String word;
+
+    private TextView txtWord;
+    private VideoView videoSinais;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word);
+
+        server = getResources().getString(R.string.server);
+        Bundle b = getIntent().getExtras();
+        word = b.getString("word");
+
+        videoSinais = (VideoView) findViewById(R.id.videoSinais);
+        txtWord = (TextView) findViewById(R.id.txtWord);
+        txtWord.setText(word);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        params.put("word", word);
+
+        try {
+            HttpManager.get(this, server + ":5000/getGif/", params, new VolleyCallback() {
+                @Override
+                public void onSuccess(JSONObject result) {
+                    String txt = null;
+                    try {
+                        txt = result.getString("link").toString();
+                        Toast.makeText(getApplicationContext(), txt, Toast.LENGTH_SHORT).show();
+
+                        Uri video = Uri.parse(txt);
+                        videoSinais.setVideoURI(video);
+                        videoSinais.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mp) {
+                                mp.setLooping(true);
+                                videoSinais.start();
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onError(JSONObject result) {
+                    Toast.makeText(getApplicationContext(), "Deu errado", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        //                            Glide.with(context)
+//                                    .load("https://inthecheesefactory.com/uploads/source/glidepicasso/gifanimation2.gif")
+//                                    .into(idDoTeuImageView);
+    }
+}
