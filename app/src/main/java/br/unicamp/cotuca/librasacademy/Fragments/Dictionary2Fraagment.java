d6d611ecb03c01ed898b0b215114b832a3b9e89c
NicolasM3 <niicck992@gmail.com>
2020-11-12 11:34:14 -0300

    dicionario pronto

--- app/src/main/java/br/unicamp/cotuca/librasacademy/Fragments/Dictionary2Fraagment.java
+++ app/src/main/java/br/unicamp/cotuca/librasacademy/Fragments/Dictionary2Fraagment.java
@@ -17,16 +17,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import br.unicamp.cotuca.librasacademy.HttpManager;
@@ -34,14 +30,15 @@ import br.unicamp.cotuca.librasacademy.R;
 import br.unicamp.cotuca.librasacademy.VolleyCallback;
 import br.unicamp.cotuca.librasacademy.WordActivity;
 
-public class DictionaryFragment extends Fragment {
-    ArrayList<String> alphabet = new ArrayList(Arrays.asList(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}));
+public class Dictionary2Fraagment extends Fragment {
+    private String letter;
+    private ArrayList<String> words = new ArrayList<>();
     private Context context;
-    private ListView itens;
-    private ImageButton search;
-    private EditText txtSearch;
-
     private String server;
+
+    private ListView lstWords;
+    private EditText txtSearch;
+    private ImageButton search;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -52,6 +49,10 @@ public class DictionaryFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle recived_letter = this.getArguments();
+        letter = recived_letter.getString("letter");
+
         server = getResources().getString(R.string.server);
     }
 
@@ -59,24 +60,53 @@ public class DictionaryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        itens = (ListView) view.findViewById(R.id.list_dictionary);
+        lstWords = (ListView) view.findViewById(R.id.list_dictionary);
         search = (ImageButton) view.findViewById(R.id.buttonSearch);
         txtSearch = (EditText) view.findViewById(R.id.edtTextSerach);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>( context, android.R.layout.simple_list_item_1, alphabet);
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("letra", letter);
 
-        itens.setAdapter(adapter);
-        itens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        HttpManager.get(
+                context, server + ":80/palavras/", params, new VolleyCallback() {
+            @Override
+            public void onSuccess(JSONObject result) {
+                try {
+                    JSONArray words_json = result.getJSONArray("palavras");
+                    for(int i = 0; i < words_json.length(); i++){
+                        words.add(words_json.getJSONObject(i).getString("palavra"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, words);
+
+                lstWords.setAdapter(adapter);
+            }
+
+            @Override
+            public void onError(JSONObject result) {
+                try {
+                    Toast.makeText(context, "erro", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(context, "erro ao lançar erro", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        lstWords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Fragment words_fragment = new Dictionary2Fraagment();
+                Intent word_data = new Intent(getActivity(), WordActivity.class);
 
-                Bundle letter = new Bundle();
-                letter.putString("letter", alphabet.get(i));
+                Bundle mWord = new Bundle();
+                mWord.putString("word", words.get(i));
 
-                words_fragment.setArguments(letter);
+                word_data.putExtras(mWord);
+                startActivity(word_data);
 
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, words_fragment).commit();
+
             }
         });
 
@@ -87,16 +117,16 @@ public class DictionaryFragment extends Fragment {
 
                 Bundle mWord = new Bundle();
                 mWord.putString("word", txtSearch.getText().toString());
-
                 word_data.putExtras(mWord);
                 startActivity(word_data);
             }
         });
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_dictionary, container, false);
+        return inflater.inflate(R.layout.activity_dictionary2, container, false);
     }
 }
